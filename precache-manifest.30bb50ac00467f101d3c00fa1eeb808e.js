17e592b4389cf68e4a0d4eb1c25daac8ad13d5c8
Locallize <Fillanlee@aol.com>
2019-08-11 16:35:27 -0400

    Updates

---
+++ precache-manifest.30bb50ac00467f101d3c00fa1eeb808e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62183ee9e5165a9bbd4250f238d0d87a",
+    "url": "/PortfolioSite/index.html"
+  },
+  {
+    "revision": "0ea89819c5af1b49aaa3",
+    "url": "/PortfolioSite/static/css/main.fce49a0c.chunk.css"
+  },
+  {
+    "revision": "0aa3a3f847a3200b7631",
+    "url": "/PortfolioSite/static/js/2.cd11cf67.chunk.js"
+  },
+  {
+    "revision": "0ea89819c5af1b49aaa3",
+    "url": "/PortfolioSite/static/js/main.9f61ae17.chunk.js"
+  },
+  {
+    "revision": "fa8dadf6d89e7c08680a",
+    "url": "/PortfolioSite/static/js/runtime~main.a84fd15a.js"
+  }
+]);
